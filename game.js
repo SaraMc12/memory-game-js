06f9fd55015b4c4e01748362774c6378a09bf5d7
Sara McMullin <mcsara1221@gmail.com>
2019-08-18 12:30:41 -0500

    refactoring plus gamboard lock created

--- game.js
+++ game.js
@@ -1,39 +1,53 @@
 const cards = document.querySelectorAll(".memory-card");
 
-let hasFlippedCard= false;
-
+let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard, secondCard;
 
-function flipCard(){
+function flipCard() {
+    if (lockBoard) return;
+
     this.classList.toggle('flip');
     // when first card is clicked
-   
-    if(!hasFlippedCard){
+
+    if (!hasFlippedCard) {
         hasFlippedCard = true;
         firstCard = this;
-    // when second card is clicked
-   
-    }else{
+        // when second card is clicked
+
+    } else {
         hasFlippedCard = false;
-        secondCard= this;
-    
-    // if the cards are a match
-    
-    if(firstCard.dataset.framework === secondCard.dataset.framework){
-        firstCard.removeEventListener("click", flipCard);
-        secondCard.removeEventListener("click", flipCard);
-    }else{
-        // if they dont match
-        setTimeout(() => {
-            
-        firstCard.classList.remove('flip');
-        secondCard.classList.remove('flip');
-        
-    }, 3000);
-    }
-       
-    
+        secondCard = this;
+
+        checkForMatch();  
     }
 }
 
-cards.forEach(card=> card.addEventListener('click', flipCard));
+function checkForMatch(){
+ // if the cards are a match
+
+ if (firstCard.dataset.framework === 
+    secondCard.dataset.framework) {
+   disableCards();
+} else {
+    // if they dont match
+    unflip();
+}
+}
+
+function disableCards(){
+    firstCard.removeEventListener("click", flipCard);
+    secondCard.removeEventListener("click", flipCard);
+}
+function unflip(){
+   
+    lockBoard = true;
+   
+    setTimeout(() => {
+
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+        lockBoard = false;
+    }, 3000);
+}
+cards.forEach(card => card.addEventListener('click', flipCard));
